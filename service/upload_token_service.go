586ee88640123e44e7616dc974c47a3c3b31c304
MrChengsiyi <15968183229@163.com>
2020-06-16 21:01:03 +0800

    "basic crud finished"

---
+++ service/upload_token_service.go
@@ -0,0 +1,73 @@
+package service
+
+import (
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/google/uuid"
+	"mime"
+	"path/filepath"
+	"singo/serializer"
+)
+
+// UploadTokenService 获得上传oss token的服务
+type UploadTokenService struct {
+	Filename string `form:"filename" json:"filename"`
+}
+
+// Post 创建token
+func (service *UploadTokenService) Post() serializer.Response {
+	client, err := oss.New("oss-cn-beijing.aliyuncs.com", "LTAI4GE2CS8jxJwbjJAB1Kvq", "qQgVjCnnak3gteeVbZeYZGNBFVjufT")
+	if err != nil {
+		return serializer.Response{
+			Code:  50002,
+			Msg:   "oss配置失败",
+			Error: err.Error(),
+		}
+	}
+
+	// 获取存储空间。
+	bucket, err := client.Bucket("csyishero")
+	if err != nil {
+		return serializer.Response{
+			Code:  50002,
+			Msg:   "oss配置失败",
+			Error: err.Error(),
+		}
+	}
+	// 获取扩展名
+	ext := filepath.Ext(service.Filename)
+
+	// 带可选参数的签名直传
+	option := []oss.Option{
+		oss.ContentType(mime.TypeByExtension(ext)),
+	}
+
+	key := "upload/avatar/" + uuid.Must(uuid.NewRandom()).String() + ext
+
+	// 签名直传。
+	signedPutURL, err := bucket.SignURL(key, oss.HTTPPut, 600, option...)
+	if err != nil {
+		return serializer.Response{
+			Code:  50002,
+			Msg:   "oss失败",
+			Error: err.Error(),
+		}
+	}
+
+	// 查看图片
+	signedGetURL, err := bucket.SignURL(key, oss.HTTPGet, 600, option...)
+	if err != nil {
+		return serializer.Response{
+			Code:  50002,
+			Msg:   "oss配置失败",
+			Error: err.Error(),
+		}
+	}
+
+	return serializer.Response{
+		Data: map[string]string{
+			"key": key,
+			"put": signedPutURL,
+			"get": signedGetURL,
+		},
+	}
+}
